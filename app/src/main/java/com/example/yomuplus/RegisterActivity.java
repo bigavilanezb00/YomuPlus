af00379a825541888498dd72576dbb7e86bac28e
Bryan <bigavilanezb00@elpuig.xeill.net>
2023-03-13 19:17:55 +0100

    register creado

---
+++ app/src/main/java/com/example/yomuplus/RegisterActivity.java
@@ -0,0 +1,92 @@
+package com.example.yomuplus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button registerButton;
+    EditText name, email, password;
+    FirebaseFirestore mFirestore;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        name = findViewById(R.id.nombreRegisterEditText);
+        email = findViewById(R.id.emailRegisterEditText);
+        password = findViewById(R.id.contrasenaRegisterEditText);
+        registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameUser = name.getText().toString().trim();
+                String emailUser = email.getText().toString().trim();
+                String passwordUser = password.getText().toString().trim();
+
+                if (nameUser.isEmpty() && emailUser.isEmpty() && passwordUser.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Complete los datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser(nameUser, emailUser, passwordUser);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String nameUser, String emailUser, String passwordUser) {
+        mAuth.createUserWithEmailAndPassword(emailUser, passwordUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String id = mAuth.getCurrentUser().getUid();
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", id);
+                map.put("name", nameUser);
+                map.put("email", emailUser);
+                map.put("password", passwordUser);
+
+                mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        finish();
+                        startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                        Toast.makeText(RegisterActivity.this, "Usuario guardado con éxito", Toast.LENGTH_SHORT).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "Error al registrar.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
