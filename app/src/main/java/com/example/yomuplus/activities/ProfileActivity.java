133be57623192fbb8a225cae40c3c8e8bff399b6
Bryan <bigavilanezb00@elpuig.xeill.net>
2023-05-18 19:18:48 +0200

    verificar cuenta

--- app/src/main/java/com/example/yomuplus/activities/ProfileActivity.java
+++ app/src/main/java/com/example/yomuplus/activities/ProfileActivity.java
@@ -1,12 +1,17 @@
 package com.example.yomuplus.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.yomuplus.MyApplication;
@@ -14,7 +19,10 @@ import com.example.yomuplus.R;
 import com.example.yomuplus.adapters.AdapterPdfFavorite;
 import com.example.yomuplus.databinding.ActivityProfileBinding;
 import com.example.yomuplus.models.ModelPdf;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,10 +36,13 @@ public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
 
     private ArrayList<ModelPdf> pdfArrayList;
     private AdapterPdfFavorite adapterPdfFavorite;
+
+    private ProgressDialog progressDialog;
 
     private static final String TAG = "PROFILE_TAG";
 
@@ -41,7 +52,19 @@ public class ProfileActivity extends AppCompatActivity {
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // reiniciamos la información del usuario
+        binding.accountTypeTv.setText("N/A");
+        binding.memberDateTv.setText("N/A");
+        binding.favoriteBookCountTv.setText("N/A");
+        binding.accountStatusTv.setText("N/A");
+
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Por favor esperé");
+        progressDialog.setCanceledOnTouchOutside(false);
+
         loadUserInfo();
         loadFavoriteBooks();
 
@@ -59,10 +82,71 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        binding.accountStatusTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firebaseUser.isEmailVerified()) {
+                    Toast.makeText(ProfileActivity.this, "Usuario verificado", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    emailVerificationDialog();
+                }
+            }
+        });
+
+    }
+
+    private void emailVerificationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Verifica correo")
+                .setMessage("¿Estás seguro de querer recibir un correo a "+firebaseUser.getEmail()+" con las instrucciones para verificar tu cuenta?")
+                .setPositiveButton("ENVIAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sendEmailVerification();
+                    }
+                })
+                .setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+    private void sendEmailVerification() {
+        progressDialog.setMessage("Enviando correo con las instrucciones para verificar a "+firebaseUser.getEmail());
+        progressDialog.show();
+
+        firebaseUser.sendEmailVerification()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ProfileActivity.this, "Instrucciones enviadas, verifique su correo "+firebaseUser.getEmail(), Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(ProfileActivity.this, "Fallo debido a "+ e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void loadUserInfo() {
         Log.d(TAG, "loadUserInfo: Cargando información de usuario del usuario "+firebaseAuth.getUid());
+
+        if (firebaseUser.isEmailVerified()) {
+            binding.accountStatusTv.setText("Verificado");
+        }
+        else {
+            binding.accountStatusTv.setText("No verificado");
+        }
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
         reference.child(firebaseAuth.getUid())
